89459dd2bd883d59851d4ca090d8f869c9088cd6
pruppets <david.weis.1010@gmail.com>
2018-02-19 00:27:29 -0700

    updating my own project, saving whatever i did to the other one which couldnt havent been much

--- test.py
+++ test.py
@@ -12,7 +12,6 @@ ibm = data.DataReader('IBM','yahoo',start=datetime.datetime(2016, 1, 1),end=date
 
 data = pd.concat([aapl['Close'], spy['Close'], ibm['Close']], axis=1)
 
-
 print('for break')
 
 analyzer = qqpat.Analizer(data, column_type='price', titles=["APPL", "SPY", "IBM"])
